06ac2d1e34ecb667d8f35a43b43eaad48ac3ff5e
filipton <filipton12@gmail.com>
2022-01-15 20:26:52 +0100

    Added admin panel and reports

--- fwShortener/Program.cs
+++ fwShortener/Program.cs
@@ -1,155 +1,288 @@
+using System.Text.RegularExpressions;
 using fwShortener;
+using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 
-SQLConnection.Init();
-if (!Directory.Exists("images"))
+public class Program
 {
-    Directory.CreateDirectory("images");
-}
+    //filip:AdminPswd123
+    //ZmlsaXA6QWRtaW5Qc3dkMTIz
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    //app.UseHsts();
-}
-
-app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
-app.Map("/Error", HandleErrorPage);
-
-app.Map("/create", HandleCreatePage);
-app.Map("/upload", HandleUploadImage);
-
-app.Map("/s", HandleShortLinks);
-app.Map("/i", HandleImages);
-
-app.MapGet("/list-s", () =>
-{
-    return SQLConnection.GerALlUrls();
-});
-
-var options = new DefaultFilesOptions();
-options.DefaultFileNames.Clear();
-options.DefaultFileNames.Add("index.html");
-app.UseDefaultFiles(options);
-
-app.UseStaticFiles();
-
-app.Run();
-
-
-static void HandleErrorPage(IApplicationBuilder app)
-{
-    app.Run(async context =>
+    public static string LoginToken = "ZmlsaXA6QWRtaW5Qc3dkMTIz";
+    
+    public static void Main(string[] args)
     {
-        string scString = context.Request.Query["statusCode"];
-        int statusCode = int.Parse(string.IsNullOrEmpty(scString) ? "500" : scString);
-
-        context.Response.StatusCode = statusCode;
-        await context.Response.WriteAsync(File.ReadAllText("wwwroot/error.html").
-            Replace("{statusCode}", statusCode.ToString()).
-            Replace("{statusDescription}", fwShortener.StatusCodes.GetStatusDescription(statusCode))
-        );
-    });
-}
-
-/*
- * Url Shortener
-*/
-#region Url Shortener
-static void HandleCreatePage(IApplicationBuilder app)
-{
-    app.Run(async context =>
-    {
-        string randomId = SQLConnection.RandomString(8);
-        string providedUrl = context.Request.Form["surl"];
-
-        if (Uri.TryCreate(providedUrl, UriKind.Absolute, out Uri? uriResult)
-            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+        SQLConnection.Init();
+        if (!Directory.Exists("images"))
         {
-            SQLConnection.SetUrl(randomId, providedUrl);
-            await context.Response.WriteAsync($"https://filipton.tech/s/{randomId} will redirect you to: {context.Request.Form["surl"]}");   
+            Directory.CreateDirectory("images");
         }
-        else
-        {
-            await context.Response.WriteAsync($"Url that you provided was not valid url!");   
-        }
-    });
-}
 
-static void HandleShortLinks(IApplicationBuilder app)
-{
-    app.Run(async context =>
-    {
-        string url = SQLConnection.GetUrl(context.Request.Path.Value.TrimStart('/'));
-        if (!string.IsNullOrEmpty(url))
-        {
-            context.Response.Redirect(url);   
-        }
-        else
-        {
-            context.Response.StatusCode = 404;
-        }
-    });
-}
-#endregion
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-/*
- * Images Server
-*/
-#region Images Server
-static void HandleUploadImage(IApplicationBuilder app)
-{
-    app.Run(async context =>
-    {
-        string randomId = SQLConnection.RandomString(8);
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            //app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
+        app.Map("/Error", HandleErrorPage);
+
+        app.Map("/create", HandleCreatePage);
+        app.Map("/upload", HandleUploadImage);
+        app.Map("/admin", HandleAdminPage);
+        app.Map("/api", HandleApi);
+
+        app.Map("/s", HandleShortLinks);
+        app.Map("/i", HandleImages);
         
-        if (context.Request.Form.Files.Count == 1)
+        app.MapGet("/list-r", SQLConnection.GetAllReports);
+
+        app.MapGet("/report", async ctx =>
         {
-            MemoryStream fileInMem = new MemoryStream();
-            context.Request.Form.Files[0].CopyTo(fileInMem);
-            fileInMem.Position = 0;
+            ctx.Response.ContentType = "text/html";
+            await ctx.Response.WriteAsync(
+                File.ReadAllText("wwwroot/report.html")
+            );
+        });
 
-            var image = Image.Load(fileInMem, out IImageFormat imageFormat);
-
-            if (imageFormat.Name.Equals("PNG") || imageFormat.Name.Equals("JPEG"))
-            {
-                await image.SaveAsJpegAsync(Path.Combine("images", randomId + ".jpg"));
-            }
-            else if (imageFormat.Name.Equals("GIF"))
-            {
-                await image.SaveAsGifAsync(Path.Combine("images", randomId + ".gif"));
-            }
-
-            await context.Response.WriteAsync($"IMAGE SAVED AS: https://filipton.tech/i/{randomId} OR https://i.filipton.tech/{randomId}");
-        }
-        else
+        app.MapPost("/report", async ctx =>
         {
-            await context.Response.WriteAsync("This method takes one file!");
-        }
-    });
-}
+            //.Replace("{ResponseMessage}", "")
+            string output = File.ReadAllText("wwwroot/report-complete.html");
 
-static void HandleImages(IApplicationBuilder app)
-{
-    app.Run(async context =>
+            string aurl = ctx.Request.Form["aurl"];
+            string amsg = ctx.Request.Form["amsg"];
+
+            if (Regex.IsMatch(aurl, "((http(s?))\\://)((i.)?)filipton.tech/"))
+            {
+                SQLConnection.AddReport(aurl, amsg);
+                output = output.Replace("{ResponseMessage}", "Thank you for reporting! We'll check it shortly!");
+            }
+            else
+            {
+                output = output.Replace("{ResponseMessage}", "You need to pass correct url!");
+            }
+
+            ctx.Response.ContentType = "text/html";
+            await ctx.Response.WriteAsync(output);
+        });
+
+        var options = new DefaultFilesOptions();
+        options.DefaultFileNames.Clear();
+        options.DefaultFileNames.Add("index.html");
+        app.UseDefaultFiles(options);
+
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+    
+    static void HandleErrorPage(IApplicationBuilder app)
     {
-        string path = context.Request.Path.ToString().TrimStart('/');
-
-        // && Directory.Exists(new FileInfo(Path.Combine("images", path)).DirectoryName)
-        if (!string.IsNullOrEmpty(path))
+        app.Run(async context =>
         {
-            string[] files = Directory.GetFiles("images", $"{path}*");
-            if (files.Length > 0)
+            string scString = context.Request.Query["statusCode"];
+            int statusCode = int.Parse(string.IsNullOrEmpty(scString) ? "500" : scString);
+
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsync((await File.ReadAllTextAsync("wwwroot/error.html")).
+                Replace("{statusCode}", statusCode.ToString()).
+                Replace("{statusDescription}", fwShortener.StatusCodes.GetStatusDescription(statusCode))
+            );
+        });
+    }
+
+    static void HandleAdminPage(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            if (context.Request.Headers.ContainsKey("Authorization") || context.Request.Cookies.ContainsKey("auth"))
             {
-                await context.Response.Body.WriteAsync(File.ReadAllBytes(files[0]));
-                return;
+                if (context.Request.Cookies["auth"] == LoginToken ||
+                    context.Request.Headers["Authorization"].ToString().Contains(LoginToken))
+                {
+                    context.Response.Headers.Add("Set-Cookie", $"auth={LoginToken}");
+                    await context.Response.WriteAsync(await File.ReadAllTextAsync("wwwroot/admin.html"));
+                }
+                else
+                {
+                    context.Response.StatusCode = 401;
+                    context.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"admin\"");
+                }
             }
-        }
-        context.Response.StatusCode = 404;
-    });
+            else
+            {
+                context.Response.StatusCode = 401;
+                context.Response.Headers.Add("WWW-Authenticate", "Basic relam=\"admin\"");
+            }
+        });
+    }
+
+    static void HandleApi(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            //endpoints.MapGet("/", () => "Api");
+            endpoints.MapGet("/links/{from:int}/{count:int}", async (HttpRequest request, HttpResponse response, int from, int count) =>
+            {
+                if (request.Cookies["auth"] == LoginToken)
+                {
+                    response.ContentType = "application/json";
+
+                    await response.WriteAsJsonAsync(await SQLConnection.GetUrls(from, count));
+                }
+                else
+                {
+                    response.StatusCode = 401;
+                }
+            });
+            endpoints.MapGet("/reports/{from:int}/{count:int}", async (HttpRequest request, HttpResponse response, int from, int count) =>
+            {
+                if (request.Cookies["auth"] == LoginToken)
+                {
+                    response.ContentType = "application/json";
+
+                    await response.WriteAsJsonAsync(await SQLConnection.GetReports(from, count));
+                }
+                else
+                {
+                    response.StatusCode = 401;
+                }
+            });
+            
+            endpoints.MapGet("/links", async () => await SQLConnection.GetUrlsCount());
+            endpoints.MapGet("/reports", async () => await SQLConnection.GetReportsCount());
+
+            endpoints.MapGet("/remove/link/{id}", async (HttpRequest request, HttpResponse response, string id) =>
+            {
+                if (request.Cookies["auth"] == LoginToken)
+                {
+                    response.ContentType = "application/json";
+
+                    await response.WriteAsync((await SQLConnection.RemoveUrl(id)).ToString());
+                }
+                else
+                {
+                    response.StatusCode = 401;
+                }
+            });
+            endpoints.MapGet("/remove/report/{rid}", async (HttpRequest request, HttpResponse response, string rid) =>
+            {
+                if (request.Cookies["auth"] == LoginToken)
+                {
+                    response.ContentType = "application/json";
+
+                    await response.WriteAsync((await SQLConnection.RemoveReport(rid)).ToString());
+                }
+                else
+                {
+                    response.StatusCode = 401;
+                }
+            });
+        });
+        app.UseStatusCodePages();
+    }
+    
+    /*
+     * Url Shortener
+    */
+    #region Url Shortener
+    static void HandleCreatePage(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            string randomId = SQLConnection.RandomString(8);
+            string providedUrl = context.Request.Form["surl"];
+
+            if (Uri.TryCreate(providedUrl, UriKind.Absolute, out Uri? uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            {
+                SQLConnection.SetUrl(randomId, providedUrl);
+                await context.Response.WriteAsync($"https://filipton.tech/s/{randomId} will redirect you to: {context.Request.Form["surl"]}");   
+            }
+            else
+            {
+                await context.Response.WriteAsync($"Url that you provided was not valid url!");   
+            }
+        });
+    }
+
+    static void HandleShortLinks(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            string url = SQLConnection.GetUrl(context.Request.Path.Value.TrimStart('/'));
+            if (!string.IsNullOrEmpty(url))
+            {
+                context.Response.Redirect(url);   
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        });
+    }
+    #endregion
+
+    /*
+     * Images Server
+    */
+    #region Images Server
+    static void HandleUploadImage(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            string randomId = SQLConnection.RandomString(8);
+            
+            if (context.Request.Form.Files.Count == 1)
+            {
+                MemoryStream fileInMem = new MemoryStream();
+                await context.Request.Form.Files[0].CopyToAsync(fileInMem);
+                fileInMem.Position = 0;
+
+                var image = Image.Load(fileInMem, out IImageFormat imageFormat);
+
+                if (imageFormat.Name.Equals("PNG") || imageFormat.Name.Equals("JPEG"))
+                {
+                    await image.SaveAsJpegAsync(Path.Combine("images", randomId + ".jpg"));
+                }
+                else if (imageFormat.Name.Equals("GIF"))
+                {
+                    await image.SaveAsGifAsync(Path.Combine("images", randomId + ".gif"));
+                }
+
+                await context.Response.WriteAsync($"IMAGE SAVED AS: https://filipton.tech/i/{randomId} OR https://i.filipton.tech/{randomId}");
+            }
+            else
+            {
+                await context.Response.WriteAsync("This method takes one file!");
+            }
+        });
+    }
+
+    static void HandleImages(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            string path = context.Request.Path.ToString().TrimStart('/');
+
+            // && Directory.Exists(new FileInfo(Path.Combine("images", path)).DirectoryName)
+            if (!string.IsNullOrEmpty(path))
+            {
+                string[] files = Directory.GetFiles("images", $"{path}*");
+                if (files.Length > 0)
+                {
+                    await context.Response.Body.WriteAsync(File.ReadAllBytes(files[0]));
+                    return;
+                }
+            }
+            context.Response.StatusCode = 404;
+        });
+    }
+    #endregion
 }
-#endregion
--- fwShortener/SQLConnection.cs
+++ fwShortener/SQLConnection.cs
@@ -9,10 +9,9 @@ public class SQLConnection
     {
         Connection = new SqliteConnection("Data Source=urls.db");
         Connection.Open();
-        
-        SqliteCommand command = Connection.CreateCommand();
-        command.CommandText = "CREATE TABLE IF NOT EXISTS urls (id varchar(12) PRIMARY KEY, url varchar(255));";
-        command.ExecuteNonQuery();
+
+        GenCommand("CREATE TABLE IF NOT EXISTS urls (id varchar(12) PRIMARY KEY, url varchar(255));").ExecuteNonQuery();
+        GenCommand("CREATE TABLE IF NOT EXISTS reports (url varchar(255), msg varchar(16384));").ExecuteNonQuery();
     }
 
     public static string GetUrl(string id)
@@ -32,13 +31,88 @@ public class SQLConnection
 
         return "";
     }
+    
+    public static async Task<bool> RemoveUrl(string id)
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"DELETE FROM urls WHERE id=$id;";
+        command.Parameters.AddWithValue("$id", id);
+        return await command.ExecuteNonQueryAsync() > 0;
+    }
+    
+    public static async Task<bool> RemoveReport(string rid)
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"DELETE FROM reports WHERE rowid=$rid;";
+        command.Parameters.AddWithValue("$rid", rid);
+        return await command.ExecuteNonQueryAsync() > 0;
+    }
 
-    public static string GerALlUrls()
+    public record UrlsRecord(string Id, string Url);
+    public static async Task <List<UrlsRecord>> GetUrls(int from = 0, int to = int.MaxValue)
+    {
+        List<UrlsRecord> urls = new List<UrlsRecord>();
+
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"SELECT * FROM urls LIMIT {from},{to};";
+        using (var reader = await command.ExecuteReaderAsync())
+        {
+            for (int i = 0; reader.Read(); i++)
+            {
+                urls.Add(new UrlsRecord(reader.GetString(0), reader.GetString(1)));
+            }
+        }
+
+        return urls;
+    }
+    
+    public record ReportRecord(string RowId, string Url, string Msg);
+    public static async Task <List<ReportRecord>> GetReports(int from = 0, int to = int.MaxValue)
+    {
+        List<ReportRecord> urls = new List<ReportRecord>();
+
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"SELECT rowid, * FROM reports LIMIT {from},{to};";
+        using (var reader = await command.ExecuteReaderAsync())
+        {
+            for (int i = 0; reader.Read(); i++)
+            {
+                urls.Add(new ReportRecord( reader.GetString(0),reader.GetString(1), reader.GetString(2)));
+            }
+        }
+
+        return urls;
+    }
+
+    public static async Task<int> GetUrlsCount()
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"SELECT COUNT(*) FROM urls;";
+        return Convert.ToInt32(await command.ExecuteScalarAsync());
+    }
+    
+    public static async Task<int> GetReportsCount()
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"SELECT COUNT(*) FROM reports;";
+        return Convert.ToInt32(await command.ExecuteScalarAsync());
+    }
+
+    public static void SetUrl(string id, string url)
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = $"INSERT INTO urls (id, url) VALUES ($id, $url);";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$url", url);
+        command.ExecuteNonQuery();
+    }
+    
+    public static string GetAllReports()
     {
         string output = "LIST: \n";
         
         SqliteCommand command = Connection.CreateCommand();
-        command.CommandText = $"SELECT * FROM urls;";
+        command.CommandText = $"SELECT * FROM reports;";
         using (var reader = command.ExecuteReader())
         {
             while (reader.Read())
@@ -49,14 +123,21 @@ public class SQLConnection
 
         return output;
     }
-
-    public static void SetUrl(string id, string url)
+    
+    public static void AddReport(string url, string msg)
     {
         SqliteCommand command = Connection.CreateCommand();
-        command.CommandText = $"INSERT INTO urls (id, url) VALUES ($id, $url);";
-        command.Parameters.AddWithValue("$id", id);
+        command.CommandText = $"INSERT INTO reports (url, msg) VALUES ($url, $msg);";
         command.Parameters.AddWithValue("$url", url);
+        command.Parameters.AddWithValue("$msg", msg);
         command.ExecuteNonQuery();
+    }
+
+    public static SqliteCommand GenCommand(string cmd)
+    {
+        SqliteCommand command = Connection.CreateCommand();
+        command.CommandText = cmd;
+        return command;
     }
     
     private static Random random = new Random();
